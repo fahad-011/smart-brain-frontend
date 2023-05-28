a655de02e9cac1beb02c9552dcb73a9dfab22899
roc102 <kyroc102@gmail.com>
2023-05-28 12:38:34 +0530

    Updates

---
+++ precache-manifest.5a25f9a906660878ced5983cf5f3acac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cd1913a65ed2cf782f9",
+    "url": "/smart-brain-frontend/static/css/main.87f85f43.chunk.css"
+  },
+  {
+    "revision": "1cd1913a65ed2cf782f9",
+    "url": "/smart-brain-frontend/static/js/main.1cd1913a.chunk.js"
+  },
+  {
+    "revision": "7fc5741dfb5d402e95fa",
+    "url": "/smart-brain-frontend/static/js/1.7fc5741d.chunk.js"
+  },
+  {
+    "revision": "77b2f96244475d3ac16d",
+    "url": "/smart-brain-frontend/static/css/2.65faa0df.chunk.css"
+  },
+  {
+    "revision": "77b2f96244475d3ac16d",
+    "url": "/smart-brain-frontend/static/js/2.77b2f962.chunk.js"
+  },
+  {
+    "revision": "f5c217ab51367923adfb",
+    "url": "/smart-brain-frontend/static/js/runtime~main.f5c217ab.js"
+  },
+  {
+    "revision": "1e92aeb2d4fc09fa997ae62f1577645d",
+    "url": "/smart-brain-frontend/index.html"
+  }
+];
